d7181fc9c034ca0d65ef1169db84d2d0b5753848
Sylvanus Currie [RWE] <71233932+s-a-c@users.noreply.github.com>
2022-11-03 23:17:22 +0000

    committed

--- .history/myngapp/src/app/square/square.component_20221103223532.ts
+++ .history/myngapp/src/app/square/square.component_20221103223532.ts
@@ -7,10 +7,10 @@ import { Component, Input } from "@angular/core";
     <button status="success" *ngIf="value == 'X'">{{ value }}</button>
     <button status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: [ 'button { width: 100%; height: 100%; color: darkviolet; font-size: clamp(1vw, 1rem + 5vw, 15vw) }' ]
+  styles: [ 'button { width: 100%; height: 100%; font-color: darkviolet; font-size: clamp(1vw, 1rem + 5vw, 15vw) }' ]
 } )
 export class SquareComponent {
 
-  @Input() value: 'X' | 'O'
+  @Input() value: 'X' | 'O' = "X";
 
 }
--- .history/myngapp/src/app/square/square.component_20221103223708.ts
+++ .history/myngapp/src/app/square/square.component_20221103223708.ts
@@ -11,6 +11,6 @@ import { Component, Input } from "@angular/core";
 } )
 export class SquareComponent {
 
-  @Input() value: 'X' | 'O'
+  @Input() value: 'X' | 'O' = "X";
 
 }
